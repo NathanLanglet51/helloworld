eb946e4d9bb7700ea983aecffd1e35a1d7eceeb2
nathan langlet <nathan-51@hotmail.fr>
2022-01-13 09:42:28 +0100

    test

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -6,8 +6,15 @@ import org.junit.jupiter.api.Test;
 
 public class AppTest {
 
+    Calcul c = new Calcul();
+
     @Test
-    public void test() {
-        assertEquals(true, true);
+    public void testAdditionPositive(){
+        assertEquals(10, c.add(5,5));
     }
-}
+
+    @Test
+    public void testAdditionNegative(){
+        assertEquals(-10, c.add(-5,-5));
+    }
+    }
